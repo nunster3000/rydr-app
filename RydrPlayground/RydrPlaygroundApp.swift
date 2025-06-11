439b9a9bcee65dde1e0a89ec608b3944611ff570
Khris <nunster3000@gmail.com>
2025-06-11 10:14:07 -0400

    Initial Commit

---
+++ RydrPlayground/RydrPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  RydrPlaygroundApp.swift
+//  RydrPlayground
+//
+//  Created by Khris Nunnally on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RydrPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
